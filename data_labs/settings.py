9aafc63a823488be7a2dbe2093951d606d8ad4b6
Lukasz Malucha <lucasmalucha@gmail.com>
2019-04-24 12:08:37 +0100

    update

--- data_labs/settings.py
+++ data_labs/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'data-labs-python.herokuapp.com', '*', ]
 
